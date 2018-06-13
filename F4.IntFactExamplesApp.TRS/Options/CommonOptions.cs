425e1acfca95dbea6af19b1b405bf760826efa7d
SergeyVYakhontov <39854074+SergeyVYakhontov@users.noreply.github.com>
2018-06-13 22:56:26 +0300

    Rename dir

---
+++ F4.IntFactExamplesApp.TRS/Options/CommonOptions.cs
@@ -0,0 +1,28 @@
+﻿////////////////////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+using Core;
+using Ninject;
+using ExistsAcceptingPath;
+using VerifyResults;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+namespace IntegerFactExamplesAppTRS
+{
+  public class CommonOptions : ICommonOptions
+  {
+    public bool CheckDataStructures => false;
+    public ulong RDASlotMaxSize => 256;
+    public float LinProgEpsilon => 0.0001F;
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
---
+++ F4.IntFactExamplesApp.TRS/Options/DebugOptions.cs
@@ -0,0 +1,24 @@
+﻿////////////////////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using Core;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+namespace IntegerFactExamplesAppTRS
+{
+  public class DebugOptions : IDebugOptions
+  {
+    public ulong muStart => 1;
+    public bool RunRDA => false;
+    public bool ComputeCommoditiesExplicitely => true;
+    public bool IntFactTestRules => false;
+    public uint FactTRS_muUpperBound => 100;
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
